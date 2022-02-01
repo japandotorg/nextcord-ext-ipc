ea96546a6b0193a2176eb675d40a8030a683daf0
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2022-02-01 21:23:34 +0530

    version updated to `0.1.1`

--- nextcord/ext/ipc/__init__.py
+++ nextcord/ext/ipc/__init__.py
@@ -6,5 +6,5 @@ from nextcord.ext.ipc.errors import *
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
 
-version = "0.1.0"
+version = "0.1.1"
 version_info = _VersionInfo(0, 1, 0, "final", 0)
